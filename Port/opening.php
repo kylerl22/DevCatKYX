3c4ab5a02468f46e1b8efd8e11e514525ce306d8
kylerl22 <kylerleasure794@gmail.com>
2017-04-07 14:02:54 -0500

    Add files via upload

---
+++ Port/opening.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet" href="css/main.css">
+
+
+
+
+</head>
+
+<body>
+
+
+
+<center><img src="Images/KYXlogo.png"></center>
+
+<center><a class="button" href="blog.html">GO &#8594;</center><a/>
+
+
+
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name">
+<br>
+<input type="submit">
+</form>
+
+
+<p>Welcome <?php echo $_POST["name"]; ?></p>
+<br>
+
+</body>
+
+</html>
